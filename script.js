48003f59b852ff197fa6d60fc06df7cf3c9b04b8
maniShankar8096 <71327182+maniShankar8096@users.noreply.github.com>
2020-12-07 21:56:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("this the test page for learning git");
+let userResponse=confirm("Do you want to proceed");
+userResponse?alert("You confirmed to proceed"):let reason=prompt("why don't you proceed");
